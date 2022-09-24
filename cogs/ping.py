058fa40d4ff8212714e45a1a339b24970d85b615
EliteNarNar <77871782+EliteNarNar@users.noreply.github.com>
2022-09-24 01:59:04 +0100

    Fixed the interactions not found error, and also added a decorator to the command function so it is recognised as a scommand

--- cogs/ping.py
+++ cogs/ping.py
@@ -1,12 +1,13 @@
-# TESTING COMMAND
+import interactions
 
-from interactions import *
+GUILD_ID = 1020765433395163168
 
 class Ping(interactions.Extension):
 
     def __init__(self, bot):
-        self.bot = bot;
-
+        self.bot = bot
+     
+    @interactions.extension_command(name="ping", scope=GUILD_ID)
     async def PingCommand(ctx: interactions.CommandContext): 
         await ctx.send("🏓 Ping Pong!  {0}".format(round(bot.latency, 1)))
 
